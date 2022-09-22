4c95d8eea102d6742f38f853b00f3b11a3d8782a
PCopter <90751258+PCopter@users.noreply.github.com>
2022-09-23 00:54:54 +0700

    ok

--- pages/api/room.js
+++ pages/api/room.js
@@ -2,4 +2,27 @@ import { readDB } from "../../backendLibs/dbLib";
 
 export default function roomRoute(req, res) {
   const rooms = readDB();
+
+  /*
+  for (const room of rooms) {
+    result.push({
+      roomId: room.roomId,
+      roomName: room.roomName,
+    });
+  }
+  */
+
+  const result = rooms.map((x) => ({
+    roomId: x.roomId,
+    roomName: x.roomName,
+  }));
+
+  if (req.method === "GET") {
+    return res.json({
+      ok: true,
+      rooms: result,
+    });
+  } else if (req.method === "POST") {
+  } else if (req.method === "DELETE") {
+  }
 }
--- pages/api/room/[roomId]/message/[messageId].js
+++ pages/api/room/[roomId]/message/[messageId].js
@@ -2,6 +2,21 @@ import { writeDB, readDB } from "../../../../../backendLibs/dbLib";
 
 export default function roomIdMessageIdRoute(req, res) {
   //read value from URL
+  const rooms = readDB();
   const roomId = req.query.roomId;
+  const roomIdx = rooms.findIndex((x) => x.roomId === roomId);
+  if (roomIdx === -1) {
+    return res.status(404).json({ ok: false, message: "Invalid room id" });
+  }
   const messageId = req.query.messageId;
+  const messageIdx = rooms[roomIdx].messages.findIndex(
+    (x) => x.messageId === messageId
+  );
+  if (messageIdx === -1) {
+    return res.status(404).json({ ok: false, message: "Invalid message id" });
+  }
+
+  rooms[roomIdx].messages.splice(messageIdx, 1);
+  writeDB(rooms);
+  return res.json({ ok: true });
 }
